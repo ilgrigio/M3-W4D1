b7e125242e7e60598617ba91f568d9988833e300
Andrea Sattin <andsattin@gmail.com>
2023-12-09 15:38:16 +0100

    Searchbar transition

---
+++ index.js
@@ -0,0 +1,8 @@
+let textInput = document.querySelector("input");
+let iconButton = document.querySelector(".bi.bi-search");
+
+iconButton.onclick = function () {
+  if (iconButton) {
+    textInput.classList.toggle("show");
+  }
+};
